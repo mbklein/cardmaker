969fc3dbc2160daab498a3dde83f235cdf5acd4b
Michael Klein <mbklein@gmail.com>
2015-04-09 09:43:01 -0500

    Switch to using the cahdmaker gem

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 gem 'sinatra'
-gem 'rmagick', :require => "RMagick"
+gem 'cahdmaker'
 gem 'json'
 
 gem 'pry', :group => :development
--- cardmaker.rb
+++ cardmaker.rb
@@ -1,9 +1,8 @@
 #!/usr/bin/env ruby
 
 require 'sinatra'
+require 'cahdmaker'
 require 'json'
-require 'RMagick'
-require 'tempfile'
 require 'uri'
 require 'base64'
 require 'zlib'
@@ -42,24 +41,8 @@ class CardMaker < Sinatra::Base
     end
 
     def make_card(text, source_card)
-      text_size = 60
-      color = source_card =~ /White/i ? 'black' : 'white'
-
-      capfile = File.join(Dir.tmpdir, Dir::Tmpname.make_tmpname(['caption','.png'],nil))
-      args = [
-        'convert', '-background', 'transparent',  '-size', '600x600', 
-        '-pointsize', text_size.to_s, '-fill', color.to_s, 
-        '-interline-spacing', (text_size/3).to_s,
-        '-font', 'Helvetica-Bold',
-        "caption:#{text}", capfile
-      ]
-      Kernel.system(*args)
-      cap = Magick::Image.read(capfile).first
-
-      img = Magick::Image.read("cards/CAH_#{source_card}.png").first
-      img.x_resolution = img.y_resolution = 300
-      img.composite!(cap, 75, 75, Magick::OverCompositeOp)
-      img.to_blob
+      maker = Cahdmaker::Maker.new
+      maker.send(source_card.downcase.to_sym, text).to_blob
     end
   end
 
